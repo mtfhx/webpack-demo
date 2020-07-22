61ff75d9cf5a9e5f09d0d98587eb4e613f99e143
HappilysHan <happilys_mtfh@sina.com>
2020-07-23 00:40:14 +0800

    Webpack configuration: Bundling assests

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './master.css'
+import './iconfont.css'
+import bgc from './bgc.jpg'
+
+console.log('Hello world')
+/*
+let img = new Image()
+img.src = bgc
+console.log(img)
+
+document.body.appendChild(img)
+*/
